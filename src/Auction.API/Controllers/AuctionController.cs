911173e7bb59d4ad31827968cc7d13e7bc59dbf5
Ricardo Pompilio <ricardopompiliodossantos@gmail.com>
2024-02-10 10:50:46 -0300

    feat: NLW lesson 1

---
+++ src/Auction.API/Controllers/AuctionController.cs
@@ -0,0 +1,26 @@
+﻿using Auction.API.Entities;
+using Auction.API.UseCases.Auctions.GetCurrent;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Auction.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuctionController : ControllerBase
+    {
+        [HttpGet]
+        [ProducesResponseType(typeof(AuctionEntity),StatusCodes.Status200OK )]
+        [ProducesResponseType(StatusCodes.Status204NoContent )]
+        public IActionResult GetCurrentAuction()
+        {
+            var useCase = new GetCurrentAuctionUseCase();
+            var result = useCase.Execute();
+
+            if (result is null) {
+                return NoContent();
+            }
+
+            return Ok(result);
+        }
+    }
+}
---
+++ src/Auction.API/Entities/AuctionEntity.cs
@@ -0,0 +1,11 @@
+﻿namespace Auction.API.Entities
+{
+    public class AuctionEntity
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public DateTime Starts { get; set; }
+        public DateTime Ends { get; set; }
+        public List<Item> Items { get; set; } = [];
+    }
+}
